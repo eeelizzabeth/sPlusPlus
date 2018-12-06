5cc6822e257f45c0391964e906319b7bb29890e7
MattCLambert <matthew@mylambert.com>
2018-12-06 15:01:14 -0800

    Add files via upload

--- assembly.cpp
+++ assembly.cpp
@@ -10,22 +10,22 @@ int main()
 {
     ifstream fin("sum.coria");      // the file the assembler will interpret
     ofstream out("sum.o");          // the file generated with 0s and 1s
-    
+
     if(fin.fail())                  // always make sure the file was opened properly
     {
         cout << "File open failed\n";
         exit(0);
     }
-    
+
     string opcode;                  // store the instruction in opcode
     int num;                        // if i need to store a number from the instruction
     string regis;                   // if i am reading a register. Note I have 8 registered
     // which i call: R1, R2, R3, R4, R5, R6, R7, and R8.
-    
+
     while(!fin.eof())                           // while not reached end of file
     {
         fin >> opcode;                          // the first line of every instruction is instruction
-        
+
         if(opcode == "onions")                  // adding function
         {
             out << "0001";                      // number one
@@ -84,7 +84,7 @@ int main()
         else if(opcode == "rumpelstiltskin")        // clear function
         {
             out << "1000";                      // number eight
-            
+
             out << "000000";                    // this is padding, since instruction have to be fixed
         }
         else if(opcode == "threeBlindMice")        // multiplication function
@@ -97,7 +97,15 @@ int main()
             }
             out << endl;            // this is padding, since instruction have to be fixed
         }
-        
+        else if(opcode == "mongo")        // multiplication function
+        {
+            out << "1010";                      // number nine
+            fin>>regis;
+            out<<reg_to_bin(regis);
+            out << "000"<<endl;            // this is padding, since instruction have to be fixed
+        }
+
+
         //        else if(opcode == "foreverAfter")       // process the OUT instruction
         //        {
         //            out << "1010";              // OUT is 111 as described in my ISA
@@ -105,17 +113,17 @@ int main()
         //            out << reg_to_bin(regis);   // this function will conver to appropriate address
         //            out << "000000";            // this is padding, since instruction have to be fixed
         //        }
-        
+
         else
         {
             cout << "ERROR: INSTRUCTION DOESN'T EXIST\n";
             exit(0);
         }
     }
-    
+
     fin.close();
     out.close();
-    
+
     return 0;
 }
 
@@ -134,7 +142,7 @@ string dec_to_binary(int dec)
             bin = "0" +bin;
         }
         dec = dec/2;
-        
+
     }
     while(bin.length() < 6)
     {
@@ -160,10 +168,14 @@ string reg_to_bin(string reg)
         return "110";
     else if(reg == "RegisterOsho")
         return "111";
-    else if(reg == "ArrayUno")
+    else if(reg == "ArrayA_Haon")
         return "000";
-    else if(reg == "ArrayDoz")
+    else if(reg == "ArrayA_Do")
         return "111";
+    else if(reg == "ArrayA_Tri")
+        return "010";
+    else if(reg == "ArrayA_Ceathair")
+        return "101";
     else
     {
         cout << "Error\n";
---
+++ simulator.cpp
@@ -0,0 +1,98 @@
+//simulator.cpp
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int bin_to_dec(string bin);
+
+int main()
+{
+    ifstream fin("sum.o");                  // the file with binary code (0s and 1s)
+
+    if(fin.fail())                          // make sure file opened okay
+    {
+        cout << "File open failed\n";
+        exit(0);
+    }
+
+    string instruction;                     // hold the entire binary instruction
+    string opcode;                          // hold the binary opcode (4 bits)
+    map<string, int> reg_map;               // a map from strings to ints used for registers
+    map<string, vector<int>> ary_map;        // a map from strings to vector<int> used for arrays
+    reg_map["000"] = 0;                     //r1
+    reg_map["001"] = 0;                     //r2
+    reg_map["010"] = 0;                     //r3
+    reg_map["011"] = 0;                     //r4
+    reg_map["100"] = 0;                     //r5
+    reg_map["101"] = 0;                     //r6
+    reg_map["110"] = 0;                     //r7
+    reg_map["111"] = 0;                     //r8
+
+    while(!fin.eof())
+    {
+        fin >> instruction;                         // read in the entire line of instrcution (13 bits)
+        opcode = instruction.substr(0,4);           // take the first 4 bits of the instruction
+
+        if(opcode == "0011") //PUT                  // if the first 4 bits are PUT
+        {
+            string n = instruction.substr(4,6);     // read the 6 bits related to numerical value
+            int val = bin_to_dec(n);                // convert binary to decimal
+
+            string regis = instruction.substr(10,3);    // find the 3 bits related to the register
+            reg_map[regis] = val;                       // use those 3 bits as location to store value
+        }
+        else if(opcode == "0101") //SUM                 // SUM instruction is followed by three registers
+        {
+            string regisA = instruction.substr(4,3);    // bits 4-6 are the first register
+            string regisB = instruction.substr(7,3);    // bits 7-9 are the second register
+            string regisC = instruction.substr(10,3);   // bits 10-12 are the last register
+
+            int sum_result = reg_map[regisA] + reg_map[regisB]; // add the first two registers
+            reg_map[regisC] = sum_result;                       // place the result in the third register
+
+        }
+        else if(opcode == "1111") //OUT                 // print to screen
+        {
+            string regis = instruction.substr(4,3);     // find which register to print
+            cout << reg_map[regis] << endl;             // use those 3 bits with map to find value.
+        }
+        else if(opcode == "1010") //MAKE THE ARRAY
+        {
+            string n = instruction.substr(4,6);         // read the 6 bits related to size of array
+            int val = bin_to_dec(n);                    // convert binary to decimal
+            string regis = instruction.substr(10,3);    // find the 3 bits related to the array
+            ary_map[regis] = vector<int>(val);
+
+            /* Quick test to make sure this works (assume vector set to size 5)
+            ary_map["000"][0] = 1;
+            ary_map["000"][1] = 3;
+            ary_map["000"][2] = 5;
+            ary_map["000"][3] = 7;
+            ary_map["000"][4] = 9;
+
+            for(int &i: ary_map["000"])
+                cout << i << endl
+            */
+        }
+
+    }
+
+    fin.close();
+
+    return 0;
+}
+
+int bin_to_dec(string bin)
+{
+    int num = 0;
+    for (int i = 0; i < bin.length(); i++)
+        if (bin[i] == '1')
+            num += pow(2, bin.length() - 1 - i);
+
+    return num;
+}
